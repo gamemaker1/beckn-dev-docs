938ef1fb2eb995d014114e6f79308df52338aee3
kiran haridas <kiranharidas187@gmail.com>
2022-03-08 22:54:11 +0530

    feat: Project page implementation

---
+++ src/components/post-card.tsx
@@ -0,0 +1,6 @@
+import { Card } from 'antd'
+import React, { Component } from 'react'
+
+export const PostCard = () => {
+	return <Card title="Card title">Card content</Card>
+}
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,11 +1,57 @@
-// src/pages/projects.tsx
-// The page that displays projects using the Beckn Protocol.
-
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { PostCard } from '../components/post-card'
+import { StaticQuery, graphql } from 'gatsby'
+import { Sidebar } from '../components/sidebar'
+import { Header } from '../components/header'
+import { Layout } from 'antd'
 
-const ProjectsPage = () => {
-	return <div>Todo</div>
+const { Sider, Content } = Layout
+
+const ProjectsPage = ({ children }: React.PropsWithChildren<{}>) => {
+	return (
+		<StaticQuery
+			query={graphql`
+				query siteTitleQueryAndSiteTitleQuery {
+					site {
+						siteMetadata {
+							title
+						}
+					}
+				}
+			`}
+			render={(data) => {
+				const { title } = data.site.siteMetadata
+				return (
+					<div>
+						<Header siteTitle={title} />
+
+						<div
+							style={{
+								display: 'grid',
+								gridTemplateColumns: 'auto 1fr auto',
+								height: '100%',
+							}}
+						>
+							{/* <Sidebar /> */}
+
+							<Layout>
+								<Content
+									style={{
+										background: '#fff',
+										padding: 24,
+										margin: 0,
+									}}
+								>
+									<PostCard />
+								</Content>
+							</Layout>
+						</div>
+					</div>
+				)
+			}}
+		/>
+	)
 }
 
 export default ProjectsPage
